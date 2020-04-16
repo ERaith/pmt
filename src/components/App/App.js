b41f80923d2786101e914fd910838f15645aa6d8
Erik Raith <raithhtiar@gmail.com>
2020-04-15 21:30:23 -0600

    [initalize] Add eslint and prettier support

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return <div>Whatsup</div>;
+  }
+}
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
