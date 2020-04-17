3942a9a4f0706ffb0b9779d0d6fd24260fd71d7b
ERaith <raithhtiar@gmail.com>
2020-04-16 18:56:54 -0600

    Revert "Feature/pokedex"

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import Pokedex from '../Pokedex/Pokedex';
 import './App.css';
 
 class App extends React.Component {
   render() {
-    return <Pokedex />;
+    return <div>Whatsup</div>;
   }
 }
 
 export default App;
+
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,28 +1,9 @@
-import React from 'react';
-import { render, waitFor, fireEvent } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { rootReducer } from '../../reducers/index';
-import { mockPokemonList } from '../../../public/mockTestFiles';
-import { fetchPokemon } from '../../apiCalls/apiCalls';
-import App from './App'
+import React from './node_modules/react';
+import { render } from './node_modules/@testing-library/react';
+import App from './App';
 
-jest.mock('../../apiCalls/apiCalls');
-
-describe('Pokedex Tests', () => {
-  // eslint-disable-next-line one-var
-  let initialState, store, testWrapper;
-  beforeEach(() => {
-    fetchPokemon.mockResolvedValue(mockPokemonList);
-    store = createStore(rootReducer);
-    testWrapper = (
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
-  });
-  it('renders the pokedex', async () => {
-    const { getByAltText } = render(testWrapper);
-    await waitFor(() => expect(getByAltText('bulbasaur')).toBeInTheDocument());
-  });
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
