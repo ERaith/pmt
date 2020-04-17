17f8f84b21c2499f6efff0784d681f00e2b0bbbb
Erik Raith <raithhtiar@gmail.com>
2020-04-17 15:27:54 -0600

    [styling] add bounce effect

--- src/components/Seachbar/Searchbar.js
+++ src/components/Seachbar/Searchbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { nameFilter } from '../../utils';
+import { nameFilter, typeFilter ,generalFilter} from '../../utils';
 import { getPokemonList, filterPokemon } from '../../actionCreators/index';
 import './Searchbar.scss';
 
@@ -9,30 +9,60 @@ class Searchbar extends React.Component {
   constructor() {
     super();
     this.state = {
-      filter: '',
+      name: '',
+      type: '',
     };
   }
 
   handleChange = (event) => {
     event.preventDefault();
-    let filteredPokemon = nameFilter(this.props.pokemonList,event.target.value)
-    this.props.filterPokemon(filteredPokemon)
+    const { name, value } = event.target;
+    let searchName = this.state.name;
+    let searchType = this.state.type;
+
+    let { pokemonList, filterPokemon,filteredPokemon} = this.props;
+    switch (name) {
+      case 'name':
+        if(value.length>searchName.length){
+          pokemonList = filteredPokemon;
+        }
+        filteredPokemon = generalFilter(pokemonList, value,searchType);
+        filterPokemon(filteredPokemon);
+        break;
+      case 'type':
+        if(value.length>searchType.length){
+          pokemonList = filteredPokemon;
+        }
+        filteredPokemon = generalFilter(pokemonList, searchName,value);
+        filterPokemon(filteredPokemon);
+        break;
+
+      default:
+        break;
+    }
+    this.setState({ [name]: value });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-
   };
 
   render() {
-    const { filter } = this.props;
+    const { name, type } = this.state;
     return (
       <form aria-label="Login Form">
         <input
           type="text"
-          placeholder="Seach Pokemon"
-          name="pokemon"
-          value={filter}
+          placeholder="Seach Name"
+          name="name"
+          value={name}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <input
+          type="text"
+          placeholder="Seach Type"
+          name="type"
+          value={type}
           onChange={(event) => this.handleChange(event)}
         />
         <button
@@ -48,9 +78,9 @@ class Searchbar extends React.Component {
   }
 }
 
-const mapStateToProps = ({ pokemonList, pokemonFiltered }) => ({
+const mapStateToProps = ({ pokemonList, filteredPokemon }) => ({
   pokemonList,
-  pokemonFiltered,
+  filteredPokemon,
 });
 
 const mapDispatchToProps = (dispatch) =>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,28 @@
-export const nameFilter = (pokemonList, filterSearch) => {
-  if(filterSearch ===""){return pokemonList}
-  filterSearch = RegExp(`^${filterSearch}`,'i')
-  let list =  pokemonList.filter((pokemon) => {
-    let name = pokemon.name;
-    return (filterSearch.test(name))
-  });
-  return list;
-}
-;
-export const typeFilter = (pokemonList, filterSearch) => {
-  if(filterSearch ===""){return pokemonList}
-  filterSearch = `^${filterSearch}`
-  let list =  pokemonList.filter((pokemon) => {
-    let name = pokemon.name;
-    return (name.match(filterSearch))
+export const filterByName = (pokemon, filterSearch) => {
+  let name = pokemon.name;
+  filterSearch = RegExp(`^${filterSearch}`, 'i');
+  let isName = filterSearch.test(name);
+  return isName;
+};
+
+export const filterByType = (pokemon, filterSearch) => {
+  let types = pokemon.types;
+  filterSearch = `^${filterSearch}`;
+  let isType = types.reduce((acc, type) => {
+    acc = acc || type.type.name.match(filterSearch);
+    return acc;
+  }, false);
+  return isType;
+};
+
+export const generalFilter = (pokemonList, nameFilter, typeFilter) => {
+  if (nameFilter === '' && typeFilter === '') {
+    return pokemonList;
+  }
+  let list = pokemonList.filter((pokemon) => {
+    let isName = filterByName(pokemon, nameFilter);
+    let isType = filterByType(pokemon, typeFilter);
+    return isName && isType;
   });
   return list;
 };
